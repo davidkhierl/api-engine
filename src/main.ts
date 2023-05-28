c2221ff478f9bb1f97ffe182a444519c71d5b3c4
David Khierl <dkrgidor@gmail.com>
2023-05-28 13:27:09 +0400

    initial prisma integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
+  /* -------------------------------------------------------------------------- */
+  /*                                    nest                                    */
+  /* -------------------------------------------------------------------------- */
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  /* -------------------------------------------------------------------------- */
+  /*                                   prisma                                   */
+  /* -------------------------------------------------------------------------- */
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 }
-bootstrap();
+
+void bootstrap();
